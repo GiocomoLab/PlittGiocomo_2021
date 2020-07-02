replicate older papers and change plotting code fix posterior normalization bug

PlaceCellAnalysis.py
@@ -125,7 +125,7 @@ def reward_cell_scatterplot(fr0, fr1, rzone0 = [250,315], rzone1 = [350,415],tma
     return f, ax
 
 
-def common_cell_remap_heatmap(fr0, fr1, rzone = [225,400], tmax= 450,bin_size=10.,norm = True):
+def common_cell_remap_heatmap(fr0, fr1, rzone = [250,400], tmax= 450,bin_size=10.,norm = True):
     '''
     plot histogram of peak locations  of place field in one environment vs the other. reward zone is highlighted
     inputs: fr0 - [pos,neurons] array of average firing rate for morph 0 trials
@@ -152,21 +152,21 @@ def common_cell_remap_heatmap(fr0, fr1, rzone = [225,400], tmax= 450,bin_size=10
     if norm:
         ax.imshow(heatmap.T/np.amax(heatmap),cmap='magma',vmin=0.05,vmax=.15)#,aspect='auto')
     else:
-        ax.imshow(heatmap.T,cmap='magma',vmax=.7*np.amax(heatmap.ravel()))
+        ax.imshow(heatmap.T,cmap='magma',vmax=np.percentile(heatmap.ravel(),99))
 
     ## add marginal histograms
     ax1 = f.add_subplot(gs[-1,0:-1],sharex=ax)
     hist,edges = np.histogram(bin_size*np.argmax(fr0,axis=0),np.arange(0,tmax+10,10))
     ax1.fill_between(np.linspace(0,45,num=edges.shape[0]-1)-.5,hist/hist.sum(),color=plt.cm.cool(1.))
     ax1.set_xlim([-1,46])
-    ax1.fill_betweenx([0,.05],rzone[0]/bin_size,x2=rzone[1]/bin_size,color='black',alpha=.2)
+    ax1.fill_betweenx([0,np.amax(hist/hist.sum())],rzone[0]/bin_size,x2=rzone[1]/bin_size,color='black',alpha=.2)
 
 
     ax2 = f.add_subplot(gs[0:-1,-1],sharey=ax)
     hist,edges = np.histogram(bin_size*np.argmax(fr1,axis=0),np.arange(0,tmax+10,10))
     ax2.fill_betweenx(np.linspace(0,45,num=edges.shape[0]-1)-.5,hist/hist.sum(),color=plt.cm.cool(0.))
     ax2.set_ylim([-1,46])
-    ax2.fill_between([0,.05],rzone[0]/bin_size,y2=rzone[1]/bin_size,color='black',alpha=.2)
+    ax2.fill_between([0,np.amax(hist/hist.sum())],rzone[0]/bin_size,y2=rzone[1]/bin_size,color='black',alpha=.2)
 
 
     return f, ax

SimulatedSimMats.py
@@ -56,7 +56,7 @@ def make_sampling_spline(xx,prob):
     return spline(cum_prob,xx)
 
 
-def simulate_session(prior,morphs, n_neurons=100,n_samps=1,rbf_sigma=.4,alpha = 1., beta = .25,xx_lims = (-.1,1.1)):
+def simulate_session(prior,morphs, n_neurons=100,n_samps=1,rbf_sigma=.4,alpha = 1., beta = .25,xx_lims = (-.3,1.3)):
     '''
     simulate a set of gamma neurons that encode samples from a posterior distribution
     inputs: prior - [1 , N] numpy array, probability mass funciton sampled evenly over interval specified in xx_lims
@@ -75,6 +75,7 @@ def simulate_session(prior,morphs, n_neurons=100,n_samps=1,rbf_sigma=.4,alpha = 
             sim_data_sf - [M, ] numpy array, similarity fraction for simulated neurons
 
     '''
+
     post, xx = calculate_posterior(prior,morphs,xx_lims=xx_lims) # calculate posterior
     neurons = np.zeros([morphs.shape[0],n_neurons*n_samps]) #allocate for neual activity
     _mun = np.linspace(xx_lims[0],xx_lims[1],num=n_neurons) # evenly space means of radial basis functions along sampling limits
@@ -176,7 +177,7 @@ def run_simmat_distributions(sess,rare_prior,freq_prior,nperms=1000,n_samps=1,rb
         print(name)
         sd,sdsm,sdsf = simmat_distribution(unity.wallmorphx(morphs),prior,nperms=nperms,n_neurons=S_trial_mat.shape[-1])
         SIMDATA[name],SIMDATA_SM[name], SIMDATA_SF[name]= sd, sdsm,sdsf
-    return morphs,S_trial_mat, np.dot(S_tmat_norm,S_tmat_norm.T), u.similarity_fraction(S_trial_mat,trial_info), SIMDATA, SIMDATA_SM, SIMDATA_SF
+    return trial_info,S_trial_mat, np.dot(S_tmat_norm,S_tmat_norm.T), u.similarity_fraction(S_trial_mat,trial_info), SIMDATA, SIMDATA_SM, SIMDATA_SF
 
 
 def simulate_session_plot_results(sess,rare_prior,freq_prior,nperms=1000,n_samps=1,rbf_sigma=.4,alpha = 1., beta = .25,xx_lims = (-.1,1.1),
@@ -199,11 +200,11 @@ def simulate_session_plot_results(sess,rare_prior,freq_prior,nperms=1000,n_samps
     epsilon = 1/nperms
 
     # run simulation
-    morphs,S_trial_mat,simmat, sf, SIMDATA,SIMDATA_SM,SIMDATA_SF = run_simmat_distributions(sess,rare_prior,freq_prior,nperms=nperms,
+    trial_info,S_trial_mat,simmat, sf, SIMDATA,SIMDATA_SM,SIMDATA_SF = run_simmat_distributions(sess,rare_prior,freq_prior,nperms=nperms,
                                                                                             n_samps=n_samps,
                                                                                             rbf_sigma=rbf_sigma,alpha = alpha, beta = beta,
                                                                                             xx_lims = xx_lims)
-
+    morph = trial_info['morphs']+trial_info['wallJitter']
     # make output directory
     sessdir = os.path.join(out_dir,"%s_%s_%i" % (sess["MouseName"],sess["DateFolder"],sess["SessionNumber"]))
     try: